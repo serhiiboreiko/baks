d5a9aaac6aa7dacc291f37fdf4df19b10ff6d091
jestersunborn <jestersunborn@gmail.com>
2019-04-10 16:53:56 +0300

    Remove request from deps. Use nodejs http module instead

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 
 // Packages
-const request = require('request');
 const colors = require('colors');
+const http = require('http');
 
 // Contsts
 const CURRENCY_SERVICE_API_KEY = 'dca86c49ab9a11316422ed8987ddb50a';
@@ -24,9 +24,20 @@ if (dolar !== 'dolar') {
   return;
 }
 
-request(`http://apilayer.net/api/live?access_key=${CURRENCY_SERVICE_API_KEY}`, (error, _, body) => {
-  if (error) throw new Error(error);
+http
+  .get(`http://apilayer.net/api/live?access_key=${CURRENCY_SERVICE_API_KEY}`, (res) => {
+    let data = '';
 
-  const { quotes: { USDUAH } } = JSON.parse(body);
-  console.log('\t', '1'.green, 'USD'.yellow, '=', USDUAH.toString().green, 'UAH'.yellow);
-});
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    res.on('end', () => {
+      const { quotes: { USDUAH } } = JSON.parse(data);
+      console.log('\t', '1'.green, 'USD'.yellow, '=', USDUAH.toString().green, 'UAH'.yellow);
+    });
+  })
+  .on('error', (error) => {
+    throw new Error(error);
+    return;
+  });
