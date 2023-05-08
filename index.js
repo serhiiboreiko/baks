bc4e338ebb33eefcdbaf73d4e0c996ab9bf92482
Andrew Voitsekhivskii <vojander@gmail.com>
2023-05-08 14:52:36 +0300

    lahidna Ukrainisation

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ const colors = require('colors');
 const getRate = require('./request');
 
 // Arguments
-const chem = process.argv[2];
+const kiko = process.argv[2];
 const dolar = process.argv[3];
 
 console.log();
 
-if (chem !== 'chem') {
-  console.log('\t', 'po chem?'.red);
+if (kiko === 'chem'){
+  console.log('\t', 'Ti sho rusnia ?'.red);
+  return;
+}
+
+if (kiko !== 'kiko') {
+  console.log('\t', 'kiko?'.red);
   return;
 }
 
 if (dolar !== 'dolar') {
-  console.log('\t', 'po chem chto?'.red);
+  console.log('\t', 'po kiko sho?'.red);
   return;
 }
 
